2781f674d55a08e18ba8de5c4910a50045576944
4l9 <111800770+4l9@users.noreply.github.com>
2022-08-23 15:53:32 +0300

    Add files via upload

---
+++ password-generator.py
@@ -0,0 +1,14 @@
+import random #import random lib
+
+
+#create the variables
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()."
+
+string = lower + upper + numbers + symbols
+length = 16
+password = "".join(random.sample(string, length))
+
+print("Your new password is: " + password)
